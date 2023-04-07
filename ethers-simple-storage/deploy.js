cdd683589744adb423c1ee4e5ab509f610b1e718
ejmorian <ejmorian@gmail.com>
2023-04-08 01:49:03 +0800

    deploy contract to sepolia testnet

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,43 +1,44 @@
-//import Ethers.js for node runtime
-const { ethers } = require("ethers");
-// package for handling files
-const fs = require("fs-extra");
-//install env package
-require("dotenv").config();
+const { ethers } = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
-
-// async function for waiting for functions that fetches an API
 async function main() {
+    // initialise rpc and wallet
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-    // connects to local ethereum network
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    // get abi and bytecode
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8")
 
-    // initialise ethereum wallet
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    // create contract
+    const contract = new ethers.ContractFactory(abi, bin, wallet)
+    console.log("deploying please wait...")
+    // deploy contract
+    const mycontract = await contract.deploy({gasLimit : 1000000})
+    console.log(`contract address : ${mycontract.address}` )
+}
 
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
-    wallet = await wallet.connect(provider);
+async function store(){
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-    const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
 
-    const contract = new ethers.ContractFactory(abi, bin, wallet);
-    const mycontract = await contract.deploy();
+    const contract = new ethers.Contract("0xDB740D6A9198BaB78bC2cC27AC826c696E8d3785", abi, wallet);
 
-    const contractTxReceipt =  await mycontract.deployTransaction.wait(1);
-    console.log(contractTxReceipt);
+    console.log("retrieving...")
+    const setNumber = await contract.setFavouriteNumber("123");
+    await setNumber.wait(1);
+    const Number = await contract.favouriteNumber();
+    console.log(`favourite number is : ${Number}`);
 
-    const currentFavouriteNumber = await mycontract.favouriteNumber();
-    console.log(`favourite number is : ${currentFavouriteNumber.toString()}`);
-    const transactionResponse = await mycontract.setFavouriteNumber("7");
-    const transactionReciept = transactionResponse.wait(1);
-    const updatedFavouriteNumber = await mycontract.favouriteNumber();
-    console.log(transactionReciept);
-    console.log(`updated favourite number : ${updatedFavouriteNumber.toString()}`);
 
 }
 
-main().then(() => process.exit(0)).catch((error) => {
-    console.log(error);
-    process.exit(1);
-})
+store()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
