b7aa68398aed2e220937a31f691d1a1a740cf05e
ejmorian <ejmorian@gmail.com>
2023-04-09 22:57:25 +0800

    add encryptedJsonKey for sepolia testnet

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111
+    }
+  }
 };
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,6 +1,12 @@
 const {ethers} = require("hardhat");
 
 const main = async () => {
+    const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    console.log("deploying contract...");
+    const simpleStorage = await simpleStorageFactory.deploy();
+    simpleStorage.deployed();
+    console.log(`succesful. contract:${simpleStorage.address}`);
+
 
 }
 
