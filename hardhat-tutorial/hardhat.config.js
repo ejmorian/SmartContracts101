f575cf63bc21eba60acf0f52e454022cae5374a4
ejmorian <ejmorian@gmail.com>
2023-04-10 12:01:22 +0800

    add gas reporter and coverage

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
 require("dotenv").config();
 const {ethers} = require("ethers");
-require("./tasks/block-number")
+require("./tasks/block-number");
+require("solidity-coverage");
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "RPC_URL"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "P_KEY"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ||  "ETHERSCAN_API_KEY"
+const CMC_API_KEY = process.env.CMC_API_KEY || "CMC_KEY"
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -26,5 +29,12 @@ module.exports = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: CMC_API_KEY,
+    outputFile: "./test/gasreport.txt",
+    noColors: true
   }
 };
